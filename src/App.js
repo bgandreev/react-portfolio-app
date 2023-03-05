45d613753fd57a0677b8bd540da1111c8cc09ff9
unknown <bgandreev@gmail.com>
2023-03-04 19:34:28 -0600

    Created a basic react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Portfolio</h1>
+        <a
+          className="App-link"
+          href="https://github.com/bgandreev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View My GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
